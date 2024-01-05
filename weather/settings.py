f0d03d24f5556e53f097778ef9cb5ef6c08c5325
gift <ikechukwugift14@gmail.com>
2024-01-05 12:51:48 +0100

    first commit

--- weather/settings.py
+++ weather/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-83@(ryv+r@)&w+f0&fy7@7$t4i(q-572-by*kkzau+3&%4j%wr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -118,10 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
