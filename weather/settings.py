7f21886ad6d056bd7ff049d983ccfba7ac67dc56
gift <ikechukwugift14@gmail.com>
2024-01-06 23:49:31 +0100

    first commit

--- weather/settings.py
+++ weather/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-83@(ryv+r@)&w+f0&fy7@7$t4i(q-572-by*kkzau+3&%4j%wr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
